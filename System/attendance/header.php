8f78854986b12635907061eb1541439993ea837e
githubnisimon <Monmon082598@gmail.com>
2018-12-04 17:26:00 +0800

    simon system update

--- System/attendance/header.php
+++
@@ -1,6 +0,0 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="style.css ">
-  <link rel="stylesheet" type="text/css" href="fonts/css/font-awesome.min.css">
-</head>
-<body  onload="startTime()">
- 
